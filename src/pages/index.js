816c2bcb544b8f56b87d1057d7e49232947e20f6
ImanRS1 <omid-omega@mail.com>
2021-05-07 13:22:28 +0200

    init navbar and footer components

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,25 @@
 import React from "react"
 
 export default function Home() {
-  return <div>HöjdenIT</div>
+  return (
+    <div>
+      HöjdenIT
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Saepe id
+        minima vel in aliquid eaque?
+      </p>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Saepe id
+        minima vel in aliquid eaque?
+      </p>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Saepe id
+        minima vel in aliquid eaque?
+      </p>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Saepe id
+        minima vel in aliquid eaque?
+      </p>
+    </div>
+  )
 }
