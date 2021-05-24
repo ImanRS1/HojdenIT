c77e373102d64681ce5a93aa23b9d0d4f5e43afd
ImanRS1 <omid-omega@mail.com>
2021-05-24 11:37:21 +0200

    conditional on view render of cable3 added

--- src/components/cableComponents/CableThree.js
+++ src/components/cableComponents/CableThree.js
@@ -3,10 +3,6 @@ import styled from "styled-components"
 import { motion } from "framer-motion"
 
 export default function CableThree(view) {
- /*  console.log(view.render) */
-  if(view.render === true){
-    console.log("ja nu")
-  }
   return (
     <CableContainer>
       <svg xmlns="http://www.w3.org/2000/svg" stroke-width="11">
@@ -14,7 +10,7 @@ export default function CableThree(view) {
           <title>Layer 1</title>
           <motion.path
             initial={{ pathLength: 0, pathOffset: 1 }}
-            animate={{ pathLength: 1, pathOffset: 0 }}
+            animate={view.render ? ({ pathLength: 1, pathOffset: 0 }) : ""}
             transition={{ duration: 0.5, delay: 3.7 }}
             id="svg_2"
             d="m156.5,123.5c-138.09055,0 -111.71989,-0.71989 -140,-1c-5.28011,-2.28011 -3,-9 -3,-80"
